e164876ec1eb029be2a03fa869954e607bd5d2ab
Jan Jarčík <jarcik.jan@gmail.com>
2019-01-16 12:48:48 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,38 @@
 #!/bin/bash    
 HOST="HOST"
-USER="USERNAME"
-PASS="PASSWORD"
+USER="USER"
+PASS="PASS"
 FTPURL="ftp://$USER:$PASS@$HOST"
 LCD="./"
-RCD="/"
+RCD="/www/"
 DELETE="--delete"
-lftp -c "set ftp:list-options -a;
-open '$FTPURL';
-lcd $LCD;
-cd $RCD;
-mirror --reverse \
-       $DELETE \
-       --verbose \
-       --exclude-glob  www/upload/ \
-       --exclude-glob  .DS_Store \
-       --exclude-glob .git/"
+
+if [ "$#" -eq  "0" ]
+    then
+        lftp -c "set ftp:list-options -a;
+        open '$FTPURL';
+        lcd $LCD;
+        cd $RCD;
+        mirror --reverse \
+            $DELETE \
+            --verbose \
+            --exclude-glob  www/upload/ \
+            --exclude-glob  .DS_Store \
+            --exclude-glob .deploy.sh \
+            --exclude-glob  node_modules/ \
+            --exclude-glob  fe/ \
+            --exclude-glob  podklady/ \
+            --exclude-glob  .vscode/ \
+            --exclude-glob  temp/ \
+            --exclude-glob  www/cms/js/lib/ \
+            --exclude-glob  log/ \
+            --exclude-glob .git/"
+    else
+        RCD="$RCD/$(dirname $1)"
+        echo "upload a file: " $1 "to" $RCD
+        lftp -c "set ftp:list-options -a;
+        open '$FTPURL';
+        lcd $LCD;
+        cd $RCD;
+        put $1"
+fi
